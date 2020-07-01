afc47000706fe02a7122a2c363fd56b219952e73
Iury Nathan <iurynathan@gmail.com>
2020-06-30 23:04:48 -0300

    Delete config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'P?{nR4SxaNC~}A*xywGhty`| N`k{mH*UXJ8|scI=1-B5$yxUh5g`{(K`WDFlF|h'
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Ecoleta'
-
-module.exports = {
-    connectionString: 'mongodb+srv://admin:possopensa3003@cluster0-ckkht.mongodb.net/Cluster0?retryWrites=true&w=majority',
-    sendgridKey: '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',
-    containerConnectionString: 'TBD'
-}
