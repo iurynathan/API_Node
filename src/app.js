67e26d85672ca6583606d77ec69eb34eaeb18633
iurynathan <iurynathan@gmail.com>
2020-07-01 09:57:43 -0300

    Correção de bug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const config = require('./config')
 
 const app = express()
 const router = express.Router()
